e8e1cbe9c26eab3c88f286042a276cf2b09879d7
LeandroSannt <lsn_slim@yahoo.com.br>
2022-10-06 15:20:25 -0300

    redirecionando para aulas

--- src/app/pages/_layouts/Dashboard/index.tsx
+++ src/app/pages/_layouts/Dashboard/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 const Dashboard:React.FC = () =>{
+  const navigate = useNavigate()
+
+  useEffect(() =>{
+    navigate('/aulas')
+  },[])
+
   return(
     <>
     <Outlet/>
