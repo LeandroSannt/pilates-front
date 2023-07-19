bd7430f0f8ad5dbe65342a255ee76641cfe4d842
LeandroSannt <lsn_slim@yahoo.com.br>
2023-07-19 16:57:31 -0300

    rotas de detalhes

---
+++ src/app/pages/Alunos/Show/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Container } from './styles';
+const Show: React.FC = () => {
+  return <Container></Container>;
+};
+export { Show };
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -10,47 +10,36 @@ import Planos from '../pages/Planos';
 import Remarcacoes from '../pages/Remarcacoes';
 import Reports from '../pages/Reports';
 import PrivateRoute from './PrivateRoute';
+import { Show } from '../pages/Alunos/Show';
 
 const AppRoutes = () => {
   return (
-        <BrowserRouter>
-          <Routes>
-            
-              <Route
-                path="/"
-                element={
-                  <Home/>
-                }
-                >
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
 
-              </Route>
-
-              <Route
-                path="/dashboard"
-                element={
-                <PrivateRoute>
-                  <Dashboard/>
-                </PrivateRoute>
-                }
-                >
-                <Route path="alunos" element={<Alunos/>}/>
-                <Route path="aulas" element={<Aulas/>}/>
-                <Route path="remarcacoes" element={<Remarcacoes/>}/>
-                <Route path="faltas" element={<Faltas/>}/>
-                <Route path="planos" element={<Planos/>}/>
-                <Route  path="planos/:id" element={ <Planos/>}/>
-                <Route path="relatorio" element={<Reports/>}/>
-                <Route path="alunos/create" element={<Details/>}/>
-                <Route path="alunos/:id/edit" element={<Details/>}/>
-              </Route>
-
-
-                
-              
-          </Routes>
-        </BrowserRouter>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          <Route path="alunos" element={<Alunos />} />
+          <Route path="detalhes" element={<Show />} />
+          <Route path="aulas" element={<Aulas />} />
+          <Route path="remarcacoes" element={<Remarcacoes />} />
+          <Route path="faltas" element={<Faltas />} />
+          <Route path="planos" element={<Planos />} />
+          <Route path="planos/:id" element={<Planos />} />
+          <Route path="relatorio" element={<Reports />} />
+          <Route path="alunos/create" element={<Details />} />
+          <Route path="alunos/:id/edit" element={<Details />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export { AppRoutes };
-
